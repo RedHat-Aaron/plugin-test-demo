2391449233079fa78764a45b467620d711589b8f
xiaoxiang.zhang <ga775@163.com>
2019-10-30 10:23:56 +0800

    init

---
+++ src/main/java/com/HelloWorld.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * @Author: xiaoxiang.zhang
+ * @Description:
+ * @Date: Create in 11:15 AM 2019/10/27
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/TestDemo.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * @Author: xiaoxiang.zhang
+ * @Description:
+ * @Date: Create in 11:06 AM 2019/10/26
+ */
+public class TestDemo {
+
+}
